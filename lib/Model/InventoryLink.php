2f885c366b29b2539f8d6b590cc2ccfbb2ff9d65
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2022-12-26 11:45:38 +0100

    [FEATURE] Allow arbitary anchors and additional dot in URL path (#12)

--- lib/Model/InventoryLink.php
+++ lib/Model/InventoryLink.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace T3Docs\Intersphinx\Model;
 
-use RuntimeException;
+use T3Docs\Intersphinx\Exception\InvalidInventoryLink;
 
 use function htmlspecialchars;
 use function preg_match;
@@ -24,8 +24,8 @@ final class InventoryLink
     ) {
         $this->project = htmlspecialchars($project);
         $this->version = htmlspecialchars($version);
-        if (preg_match('/^([a-zA-Z0-9-_]+\/)*([a-zA-Z0-9-_])+\.html(#[a-zA-Z0-9-_]*)?$/', $path) < 1) {
-            throw new RuntimeException('Inventory link "' . $path . '" has an invalid scheme. ', 1671398986);
+        if (preg_match('/^([a-zA-Z0-9-_.]+\/)*([a-zA-Z0-9-_.])+\.html(#[^#]*)?$/', $path) < 1) {
+            throw new InvalidInventoryLink('Inventory link "' . $path . '" has an invalid scheme. ', 1671398986);
         }
 
         $this->path  = $path;
